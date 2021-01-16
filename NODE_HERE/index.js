f1dbe6cbc3acde23a4aa552cbda91f91bb06ce8e
rbrtknwls <r2knowle@uwaterloo.ca>
2021-01-16 14:49:21 -0500

    almost done

--- NODE_HERE/index.js
+++ NODE_HERE/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var path = require("path");
 var aws = require("aws-sdk")
+var imageToBase64 = require('image-to-base64');
 // CONSTANTS AND API KEYS
 const PORT = process.env.PORT || 3000;
 aws.config.loadFromPath('./config.json');
@@ -12,6 +13,7 @@ s3 = new aws.S3();
 rek  = new aws.Rekognition();
 const collectionName = "crimedata"
 const bucketName = "crimedatabase"
+var currcount = 0;
 
 app.use(express.static(__dirname + "/public"));
 
@@ -26,50 +28,73 @@ console.log("CHECKING PORT " + PORT);
 init()
 function init (){
 	rek.deleteCollection({CollectionId: collectionName}, function(err, data) {
-	   if (err){
-			  console.log("No Collection Found");
-				makecollection();
-		 }else{
-			  console.log("DELETED COLLECTION: " + collectionName);
-				makecollection();
-		 }
-  });
+		if (err){
+			console.log("No Collection Found");
+			makecollection();
+		}else{
+			console.log("DELETED COLLECTION: " + collectionName);
+			makecollection();
+		}
+	});
 }
 
 function makecollection(){
 	rek.createCollection({CollectionId: collectionName}, function(err, data) {
-   if (err){
-		 console.log(err, err.stack);
-	 } else {
-		 console.log("Made Collection: "+ collectionName);
-		 gets3();
-	 }
- });
+		if (err){
+			console.log(err, err.stack);
+		} else {
+			console.log("Made Collection: "+ collectionName);
+			gets3();
+		}
+	});
 }
 
 function gets3 (){
 	s3.listObjects({Bucket: bucketName, MaxKeys: 100}, function(err, data) {
 		if (err){
-			 console.log(err, err.stack);
+			console.log(err, err.stack);
 		}else{
-			 console.log(data);
-			 appendToCollection(data.Contents)
-		 }
+			appendToCollection(data.Contents)
+		}
 	});
 }
 
 function appendToCollection(data){
 	for (var i = 0; i < data.length; i ++){
-		console.log(data[i])
-		console.log("----");
+		var locparams = {
+			CollectionId: collectionName,
+			Image: { /* required */
+				S3Object: {
+					Bucket: bucketName,
+					Name: data[i].Key
+				}
+			},
+		}
+		rek.indexFaces(locparams, function(err, img) {
+			if (err){
+				 console.log(err, err.stack);
+			 }
+			else{
+				currcount++;
+				if (currcount == data.length-1){
+					console.log("Just being safe...")
+					setTimeout(function(){
+						listFaces(collectionName);
+					}, 1000);
+				}
+			 }
+		});
 	}
 }
 
+function compareface (face){
+
+}
 
 function listFaces (colid){
 	var params = {
-	 CollectionId: colid,
-	 MaxResults: 20
+		CollectionId: colid,
+		MaxResults: 20
 	};
 	rek.listFaces(params, function(err, data) {
 		if (err) console.log(err, err.stack);
