2d88f2c0e2967cbdc44d6fc4dd65abda13a13df1
annxiesun <56134577+annxiesun@users.noreply.github.com>
2021-01-16 18:14:57 -0500

    Update App.tsx

--- REACT_NATIVE_HERE/App.tsx
+++ REACT_NATIVE_HERE/App.tsx
@@ -1,23 +1,87 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
-export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+import UploadScreen from './screens/UploadScreen';
+import ResultScreen from './screens/ResultScreen';
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
+import React, { Component } from 'react';
+import * as ImagePicker from 'expo-image-picker';
+
+const demo_person = {
+  name:"Carlos",
+  desc: "parking ticket haha"
+}
+class App extends React.Component {
+  constructor() {
+  super();
+  
+  this.state ={ 
+    page: 1,
+    image:"",
+    person: demo_person,
+    colorScheme: true
+  };
+
+   }
+
+   changetoResult(event){
+    this.setState({
+      page:2
+      /*person:null*/});
+  
+
+  }
+
+  async getImage(){
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+
+    if (!result.cancelled) {
+
+      let person = demo_person;
+      /***** FIND MATCH FUNCTION HERE replace demo_person*/
+
+      this.setState({
+        image:result.uri,
+        person:person});
+    }
+
+
+  }
+
+   changetoUpload(event){
+    this.setState({
+      page:1, 
+      /*person:null*/});
+
+  }
+
+  render() {
+    let curr_page;
+
+    if (this.state.page == 1){
+      
+      curr_page = <UploadScreen image={this.state.image} getImage ={this.getImage.bind(this)} handlePress={this.changetoResult.bind(this)}/>
+    } else if (this.state.page ==2){
+      curr_page = <ResultScreen person={this.state.person} handlePress={this.changetoResult.bind(this)}/>
+    }
+    
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      {curr_page}
+    </SafeAreaProvider>
+
     );
   }
 }
+
+export default App;
