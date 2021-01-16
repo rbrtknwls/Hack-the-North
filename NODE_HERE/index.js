f0ae039f0b8ef9638d68b130e37feaf47eaca2c0
briantu <brianptu2@gmail.com>
2021-01-16 18:50:05 -0500

    post

--- NODE_HERE/index.js
+++ NODE_HERE/index.js
@@ -1,75 +1,83 @@
 var express = require("express");
 var path = require("path");
-var aws = require("aws-sdk")
-const fs = require('fs');
+var aws = require("aws-sdk");
+const fs = require("fs");
 // CONSTANTS AND API KEYS
 const PORT = process.env.PORT || 3000;
-aws.config.loadFromPath('./config.json');
+aws.config.loadFromPath("./config.json");
 
 // Instancate OBJECTS
 var app = express();
 var server = require("http").createServer(app);
 s3 = new aws.S3();
-rek  = new aws.Rekognition();
-const collectionName = "crimedata"
-const bucketName = "crimedatabase"
+rek = new aws.Rekognition();
+const collectionName = "crimedata";
+const bucketName = "crimedatabase";
 var currcount = 0;
 
-const file = 'test1.jpg';
+const file = "test1.jpg";
 const bitmap = fs.readFileSync(file);
-const buffer = new Buffer.from(bitmap, 'base64')
+const buffer = new Buffer.from(bitmap, "base64");
 
 app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+app.use(express.urlencoded());
 
 // PAGE BUILDING STUFF
 app.get("/", function(req, res) {
 	res.sendFile(path.join(__dirname + "/index.html"));
 });
 
+app.post("/post", function(req, res) {
+	console.log(req.body);
+});
+
 server.listen(PORT);
 console.log("CHECKING PORT " + PORT);
 
-init()
+init();
 // --------------- GET STUFFFF ---------------//
-
 
 // --------------- RUNTIME STUFFFF ---------------//
 
-function init (){
-	rek.deleteCollection({CollectionId: collectionName}, function(err, data) {
-		if (err){
+function init() {
+	rek.deleteCollection({ CollectionId: collectionName }, function(err, data) {
+		if (err) {
 			console.log("No Collection Found");
 			makecollection();
-		}else{
+		}
+		else {
 			console.log("DELETED COLLECTION: " + collectionName);
 			makecollection();
 		}
 	});
 }
 
-function makecollection(){
-	rek.createCollection({CollectionId: collectionName}, function(err, data) {
-		if (err){
+function makecollection() {
+	rek.createCollection({ CollectionId: collectionName }, function(err, data) {
+		if (err) {
 			console.log(err, err.stack);
-		} else {
-			console.log("Made Collection: "+ collectionName);
+		}
+		else {
+			console.log("Made Collection: " + collectionName);
 			gets3();
 		}
 	});
 }
 
-function gets3 (){
-	s3.listObjects({Bucket: bucketName, MaxKeys: 100}, function(err, data) {
-		if (err){
+function gets3() {
+	s3.listObjects({ Bucket: bucketName, MaxKeys: 100 }, function(err, data) {
+		if (err) {
 			console.log(err, err.stack);
-		}else{
-			appendToCollection(data.Contents)
+		}
+		else {
+			appendToCollection(data.Contents);
 		}
 	});
 }
 
-function appendToCollection(data){
-	for (var i = 0; i < data.length; i ++){
+function appendToCollection(data) {
+	for (var i = 0; i < data.length; i++) {
 		var locparams = {
 			CollectionId: collectionName,
 			ExternalImageId: data[i].Key,
@@ -78,18 +86,18 @@ function appendToCollection(data){
 					Bucket: bucketName,
 					Name: data[i].Key
 				}
-			},
-		}
+			}
+		};
 		rek.indexFaces(locparams, function(err, img) {
-			if (err){
+			if (err) {
 				console.log(err, err.stack);
 			}
-			else{
+			else {
 				currcount++;
-				if (currcount == data.length-1){
-					console.log("Just being safe...")
-					setTimeout(function(){
-						compareface(buffer)
+				if (currcount == data.length - 1) {
+					console.log("Just being safe...");
+					setTimeout(function() {
+						compareface(buffer);
 					}, 1000);
 				}
 			}
@@ -97,47 +105,47 @@ function appendToCollection(data){
 	}
 }
 
-function compareface (face){
+function compareface(face) {
 	var params = {
-	  CollectionId: collectionName,
-	  Image: {
-	    Bytes: face
-	  },
-	  FaceMatchThreshold: 0,
-	  MaxFaces: 1,
+		CollectionId: collectionName,
+		Image: {
+			Bytes: face
+		},
+		FaceMatchThreshold: 0,
+		MaxFaces: 1
 	};
 	rek.searchFacesByImage(params, function(err, data) {
-	  if (err){
+		if (err) {
 			console.log(err, err.stack);
 		}
-
-	  else{
-			if (data.FaceMatches.length == 0){
+		else {
+			if (data.FaceMatches.length == 0) {
 				notfound();
-			}else{
-				found(data.FaceMatches)
+			}
+			else {
+				found(data.FaceMatches);
 			}
 		}
 	});
 }
 
 // --------------- DEBUG STUFFFF ---------------//
-function listFaces (colid){
+function listFaces(colid) {
 	var params = {
 		CollectionId: colid,
 		MaxResults: 90
 	};
 	rek.listFaces(params, function(err, data) {
 		if (err) console.log(err, err.stack);
-		else     console.log(data);
-	})
+		else console.log(data);
+	});
 }
 
 // --------------- POST STUFFFF ---------------//
-function notfound(){
-	console.log("FACE NOT FOUND")
+function notfound() {
+	console.log("FACE NOT FOUND");
 }
-function found(data){
-	console.log("FACE MATCH (s):")
-	console.log(data)
+function found(data) {
+	console.log("FACE MATCH (s):");
+	console.log(data);
 }
