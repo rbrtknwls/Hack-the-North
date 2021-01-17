b822013e185c9c0e751babcd78ff3f56723c25f2
annxiesun <56134577+annxiesun@users.noreply.github.com>
2021-01-17 00:05:41 -0500

    front-end merge

--- REACT_NATIVE_HERE/screens/UploadScreen.tsx
+++ REACT_NATIVE_HERE/screens/UploadScreen.tsx
@@ -1,46 +1,70 @@
 import React, { Component } from 'react'
-import { View, Text, Button, Image, TouchableOpacity,} from 'react-native';
+import { Animated, View, Text, Button, Image, TouchableOpacity, } from 'react-native';
+import FadeInOut from 'react-native-fade-in-out';
 
 class UploadScreen extends Component {
-  constructor(props) {
-    super(props);
-   }
+    constructor(props) {
+            super(props);
+
+        this.state = {
+            visible: false,
+            imagevisible: false,
+
+        }
 
 
-  render() {
-      let display_photo = (this.props.image!=null)  ?  <Image source={this.props.image}  style={{ width: 200, height: 200,marginBottom:50,marginTop:25 }}/>: null;
-      let textVisible = (this.props.image==null) ? "flex" : "none"
+    }
 
-    return (
-        <View style={WrapperStyle}>
+    componentDidMount() {
 
-            <View style={{... TextWrap, display:textVisible}}>
-            <Image source={require('../assets/images/bird.png')} style={{width:100, height:100}}/>
-            <Text style={Title}>RAVEN</Text>
-    <Text style={Desc}>Find out criminal information of a person from just a photo</Text>
+        setTimeout(() => {
+            this.setState({ imagevisible: true })
+            setTimeout(() => { this.setState({ visible: true }) }, 500);
+
+        }, 1000);
+
+    }
+
+
+
+    render() {
+        let display_photo = (this.props.image != null) ? <Image source={this.props.image} style={{ width: 200, height: 200, marginBottom: 50, marginTop: 25 }} /> : null;
+        let textVisible = (this.props.image == null) ? "flex" : "none"
+
+        return (
+            <View style={{ ...WrapperStyle }} >
+
+
+                <FadeInOut visible={this.state.visible}>
+                    <View style={{ ...TextWrap, display: textVisible }}>
+
+                        <FadeInOut visible={this.state.imagevisible}>
+                            <Animated.Image source={require('../assets/images/bird.png')} style={{ width: 100, height: 100 }} />
+                        </FadeInOut>
+                        <Text style={Title}>RAVEN</Text>
+                        <Text style={Desc}>Find out criminal information of a person from just a photo</Text>
+                    </View></FadeInOut>
+
+                {display_photo}
+
+                <FadeInOut visible={this.state.visible}>
+                    <View style={row1}>
+                        <TouchableOpacity title="Upload" onPress={this.props.getImage} style={UploadStyle}>
+                            <Text style={UploadText} >Upload</Text>
+                        </TouchableOpacity>
+
+                        <FadeInOut visible={(textVisible == "flex") ? false : true}>
+                            <TouchableOpacity title="Confirm" onPress={this.props.handlePress} style={{ ...ConfirmStyle, display: (textVisible == "flex") ? "none" : "flex" }}>
+                                <Text style={GoText}> Go</Text>
+                            </TouchableOpacity>
+                        </FadeInOut>
+                    </View>
+                    </FadeInOut>
+                
             </View>
 
-            {display_photo}
-
-<View style={row1}>
-<TouchableOpacity title="Upload" onPress={this.props.getImage} style={UploadStyle}>
-<Text style={UploadText} >Upload</Text>
-</TouchableOpacity>
-
-<TouchableOpacity title="Confirm" onPress={this.props.handlePress} style={{... ConfirmStyle, display: (textVisible=="flex") ? "none":"flex"}}>
-<Text style={GoText}> Go</Text>
-</TouchableOpacity>
-</View>
-
-<View>
-<TouchableOpacity title="Take photo" style={TakePhotoStyle} onPress={this.props.takeImage}>
-<Text style={PhotoText}>Take Photo</Text>
-</TouchableOpacity>
-</View>
-</View>
-
-    );
-  }
+        );
+    }
 }
 
 export default UploadScreen;
@@ -59,28 +83,28 @@ const Title= {
 
 }
 
-const Desc= {
-    color:"white",
-    fontFamily:"Futura",
-    fontSize:16,
-    marginBottom:50,
+const Desc = {
+    color: "white",
+    fontFamily: "Futura",
+    fontSize: 16,
+    marginBottom: 50,
 }
 const UploadText = {
-    color:"white",
-    fontFamily:"Futura",
-    letterSpacing:2,
+    color: "white",
+    fontFamily: "Futura",
+    letterSpacing: 2,
 }
 
 const GoText = {
-    color:"white",
-    fontFamily:"Futura",
-    letterSpacing:2,
+    color: "white",
+    fontFamily: "Futura",
+    letterSpacing: 2,
 }
 const PhotoText = {
     textDecorationLine: 'underline',
-    color:"white",
-    fontFamily:"Futura",
-    letterSpacing:2,
+    color: "white",
+    fontFamily: "Futura",
+    letterSpacing: 2,
 }
 const WrapperStyle = {
     backgroundColor: "#1C0F40",
@@ -99,7 +123,7 @@ const UploadStyle = {
     borderRadius: 15,
     paddingVertical: 10,
     paddingHorizontal: 50,
-    marginRight:10,
+    marginRight: 10,
 
 }
 
@@ -110,7 +134,7 @@ const ConfirmStyle = {
     borderRadius: 10,
     paddingVertical: 10,
     paddingHorizontal: 12,
-    marginTop:1,
+    marginTop: 1,
     marginBottom: 1,
 }
 
@@ -120,6 +144,8 @@ const TakePhotoStyle = {
     borderRadius: 10,
     paddingVertical: 10,
     paddingHorizontal: 12,
-    marginTop:1,
+    marginTop: 1,
     marginBottom: 1,
 }
+
+
