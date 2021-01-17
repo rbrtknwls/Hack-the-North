b18e3cb09f64faea68dcf6dfbdba87859987f421
annxiesun <56134577+annxiesun@users.noreply.github.com>
2021-01-17 00:17:01 -0500

    fix

--- REACT_NATIVE_HERE/App.tsx
+++ REACT_NATIVE_HERE/App.tsx
@@ -50,7 +50,7 @@ class App extends React.Component {
 
     if (ImagePicker.getMediaLibraryPermissionsAsync()){
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
